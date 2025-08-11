4460b14e0cdd03474b3a4884768de539d15184a6
Anton Begehr <anton@begehr.me>
2025-08-11 21:22:28 +0200

    mv sandbox

---
+++ app/components/Sandbox.tsx
@@ -0,0 +1,32 @@
+import {
+  ConsoleTemplate,
+  FullScreenContainer,
+  ThemeProvider,
+} from "@pipecat-ai/voice-ui-kit";
+
+// Import recommended fonts
+import "@fontsource-variable/geist";
+import "@fontsource-variable/geist-mono";
+
+// Import voice-ui-kit styles
+import "@pipecat-ai/voice-ui-kit/styles.css";
+
+interface SandboxProps {
+  connectionUrl: string;
+}
+
+export default function Sandbox({ connectionUrl }: SandboxProps) {
+  return (
+    <ThemeProvider>
+      <FullScreenContainer>
+        <ConsoleTemplate
+          transportType="smallwebrtc"
+          connectParams={{
+            connectionUrl,
+          }}
+          noUserVideo={true}
+        />
+      </FullScreenContainer>
+    </ThemeProvider>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,8 @@
-import {
-  ConsoleTemplate,
-  FullScreenContainer,
-  ThemeProvider,
-} from "@pipecat-ai/voice-ui-kit";
 import { useSearchParams } from "react-router";
+import Sandbox from "../components/Sandbox";
 
-// Import recommended fonts
-import "@fontsource-variable/geist";
-import "@fontsource-variable/geist-mono";
-
-// Import voice-ui-kit styles
-import "@pipecat-ai/voice-ui-kit/styles.css";
+// Make this route client-side only
+export const clientOnly = true;
 
 export function meta() {
   return [
@@ -33,17 +25,5 @@ export default function Home() {
     ...(key && { key }),
   }).toString()}`;
 
-  return (
-    <ThemeProvider>
-      <FullScreenContainer>
-        <ConsoleTemplate
-          transportType="smallwebrtc"
-          connectParams={{
-            connectionUrl,
-          }}
-          noUserVideo={true}
-        />
-      </FullScreenContainer>
-    </ThemeProvider>
-  );
+  return <Sandbox connectionUrl={connectionUrl} />;
 }
